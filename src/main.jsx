608ff4bc5c5f2d1d487f55aeb9770974d0a59dac
troy452 <miralbenizi7@gmail.com>
2025-11-13 10:00:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom"; // HashRouter for GH Pages
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
 
