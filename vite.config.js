e4451b285913adc127a5afcb8c74f9628f8fb4fc
troy452 <miralbenizi7@gmail.com>
2025-11-13 10:14:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR_REPO_NAME/",   // IMPORTANT for GitHub Pages
+  base: "/Gamebounty/",   // IMPORTANT for GitHub Pages
 });
 
